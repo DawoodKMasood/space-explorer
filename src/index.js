70d84341777056cbba834333ad961f0797df2930
dmasood-aslase <141400255+dmasood-aslase@users.noreply.github.com>
2023-09-25 18:15:13 +0500

    Adding background

--- src/index.js
+++ src/index.js
@@ -26,12 +26,13 @@ const game = new Phaser.Game(config);
 var ship;
 var targetX;
 var targetY;
-var backgrounds = [];
+var background;
 var shipCoordinates;
 
 function preload ()
 {
     this.load.setBaseURL('http://localhost:8080');
+    this.load.image('back', 'background/back.png');
     this.load.image('warrior1', 'objects/ships/warrior1.png');
     this.load.image('smoke', 'objects/smokes/explosion00.png');
     this.load.bitmapFont('nokia16', 'fonts/nokia16.png', 'fonts/nokia16.xml');
@@ -39,36 +40,39 @@ function preload ()
 
 function create ()
 {
-  const particles = this.add.particles(0, 0, 'smoke', {
-    quantity: 5,
-    scale: { start: 0.01, end: 0.001 },
-  });
+    background = this.add.tileSprite(0, 0, config.width * 2, config.height * 2, 'back');
+    background.setOrigin(0, 0);
+    
+    const particles = this.add.particles(0, 0, 'smoke', {
+      quantity: 5,
+      scale: { start: 0.01, end: 0.001 },
+    });
 
-  ship = this.physics.add.sprite(400, 300, 'warrior1');
-  // Set the scale of the sprite
-  ship.setScale(0.1, 0.1);
-  // Enable input for the sprite so that it can interact with the mouse
-  ship.setInteractive();
+    ship = this.physics.add.sprite(400, 300, 'warrior1');
+    // Set the scale of the sprite
+    ship.setScale(0.1, 0.1);
+    // Enable input for the sprite so that it can interact with the mouse
+    ship.setInteractive();
 
-  // Add a pointer move event to update the sprite's rotation
-  this.input.on('pointermove', function (pointer) {
-    // Calculate the angle between the sprite and the mouse pointer
-    var angle = Phaser.Math.Angle.Between(ship.x, ship.y, pointer.worldX, pointer.worldY);
-    // Convert radians to degrees and set the sprite's angle
-    ship.setAngle(Phaser.Math.RAD_TO_DEG * angle);
+    // Add a pointer move event to update the sprite's rotation
+    this.input.on('pointermove', function (pointer) {
+      // Calculate the angle between the sprite and the mouse pointer
+      var angle = Phaser.Math.Angle.Between(ship.x, ship.y, pointer.worldX, pointer.worldY);
+      // Convert radians to degrees and set the sprite's angle
+      ship.setAngle(Phaser.Math.RAD_TO_DEG * angle);
 
-    targetX = pointer.worldX;
-    targetY = pointer.worldY;
-  }, this);
+      targetX = pointer.worldX;
+      targetY = pointer.worldY;
+    }, this);
 
-  // this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-  // Set the camera to follow the ship
-  this.cameras.main.startFollow(ship);
+    // this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    // Set the camera to follow the ship
+    this.cameras.main.startFollow(ship);
 
-  particles.startFollow(ship);
+    particles.startFollow(ship);
 
-  shipCoordinates = this.add.bitmapText(0, 0, 'nokia16').setScrollFactor(0);
- 
+    shipCoordinates = this.add.bitmapText(0, 0, 'nokia16').setScrollFactor(0);
+    
 }
 
 function update() {
@@ -96,6 +100,8 @@ function update() {
     ship.setVelocity(0, 0)
   }
 
-  
+  // // Update the background position to create the illusion of an infinite map
+  background.tilePositionX = ship.x * 2; // Adjust the factor as needed
+  background.tilePositionY = ship.y * 2; // Adjust the factor as needed
 }
 
